b31bbc5552f04b1de4fb8b202db8b45e753edfbd
Mind67130705 <67130705@dpu.ac.th>
2025-08-03 13:01:42 +0700

    Update edit.php

--- src/edit.php
+++ src/edit.php
@@ -2,6 +2,6 @@
 $conn = new mysqli("db", "student", "studentpass", "finalexam");
 $id = $_GET['id'];
 $title = $_POST['title']; $link = $_POST['pdf_link'];
-$conn->query("UPDATE references SET title='$title', pdf_link='$link' WHERE id=$id");
+$conn->query("UPDATE research_references SET title='$title', pdf_link='$link' WHERE id=$id");
 header("Location: reference.php");
 ?>
